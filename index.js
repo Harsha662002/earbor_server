ecf04887314ffa5f802472a8f073fb7005022100
Harsha662002 <hvasireddy329@gmail.com>
2023-08-07 11:18:23 +0530

    Updated vercel deploy

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const pool = require("./db");
 const bcrypt = require("bcrypt");
 const router = require("express").Router();
+var pg = require("pg");
 
 //middleware
 
@@ -12,6 +13,24 @@ app.use(cors());
 app.use(express.json());
 
 //
+
+var conString =
+  "postgres://janpmiwo:UDp-DIyOC5Yrd7ejQWKsACQr3XrdH6Z7@bubble.db.elephantsql.com/janpmiwo"; //Can be found in the Details page
+var client = new pg.Client(conString);
+client.connect(function (err) {
+  if (err) {
+    return console.error("could not connect to postgres", err);
+  }
+  client.query('SELECT NOW() AS "theTime"', function (err, result) {
+    if (err) {
+      return console.error("error running query", err);
+    }
+    console.log(result.rows[0].theTime);
+    // >> output: 2018-08-23T14:02:57.117Z
+    client.end();
+  });
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
